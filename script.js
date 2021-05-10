1211a859af71a5a616283a1a13db3a94060df87c
YemmyAde <yemisadesanya@gmail.com>
2021-05-10 09:30:08 +0100

    second commit: fixed display message ; one more commit to go

--- script.js
+++ script.js
@@ -8,18 +8,42 @@ messagePass.innerHTML = `"Message appears here"`;
 function buttonClicked(){
 
  messagePass.innerHTML = message.value;
-   
-//  message.value= " ";
-
+ 
  if(message.value === "" || message.value == "null"){
 
-     emptyMessage.innerHTML = "Cant Submit Empty message";
-     emptyMessage.style.display = "block";
-     messagePass.innerHTML = `"Message appears here"`;
-     return false; // to cancel submit
- }
-    else{
-    emptyMessage.style.display = "none";
-}
+         emptyMessage.innerHTML = "Cant Submit Empty message";
+         emptyMessage.style.display = "block";
+         messagePass.innerHTML = `"Message appears here"`;
+         console.log("okay")
+        //  return false;
+     }
+
+
+ setTimeout( 'displayNone()', 2000 );
+
+
+   
+//  message.value= "";
+
+//  if(message.value === "" || message.value == "null"){
+
+//      emptyMessage.innerHTML = "Cant Submit Empty message";
+//      emptyMessage.style.display = "block";
+//      messagePass.innerHTML = `"Message appears here"`;
+//      return false;
+//  }
+//     else{
+//     emptyMessage.style.display = "none";
+// }
 
 };
+
+function displayNone(){
+     if(message.value === "" || message.value == "null"){
+
+     emptyMessage.style.display = "none";
+     messagePass.innerHTML = `"Message appears here"`;
+     console.log("haaaa")
+     return false;""
+ 
+}};
